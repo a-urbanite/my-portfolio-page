2bdff57e570cc9f5ae15aaecb961e34cf27a389c
a.urbanite <a.urbanite@gmail.com>
2022-10-11 17:15:25 +0200

    added styling for hamburger menu

--- components/HamburgerMenu/HamburgerMenu.js
+++ components/HamburgerMenu/HamburgerMenu.js
@@ -12,7 +12,7 @@ const HamburgerMenu = () => {
   return (
     <Menu as="div" className={styles.hamContainer}>
       <Menu.Button className={styles.button}><HamburgerIcon/></Menu.Button>
-      <Transition
+      {/* <Transition
           as={Fragment}
           enter="transition ease-out duration-100"
           enterFrom="transform opacity-0 scale-95"
@@ -20,26 +20,25 @@ const HamburgerMenu = () => {
           leave="transition ease-in duration-75"
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
-        >
+        > */}
         <Menu.Items className={styles.menu}>
-          <Menu.Item>
+          <Menu.Item className={styles.menuItem}>
               <span onClick={() => scrollTo("headerSection")}>Intro</span>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item className={styles.menuItem}>
               <span onClick={() => scrollTo("projectsSection")}>Projects</span>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item className={styles.menuItem}>
               <span onClick={() => scrollTo("timelineSection")}>Timeline</span>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item className={styles.menuItem}>
               <span onClick={() => scrollTo("skillsSection")}>Skills</span>
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item className={styles.menuItem}>
               <span onClick={() => scrollTo("contactSection")}>Contact</span>
           </Menu.Item>
-
         </Menu.Items>
-      </Transition>
+      {/* </Transition> */}
     </Menu>
   )
 }
