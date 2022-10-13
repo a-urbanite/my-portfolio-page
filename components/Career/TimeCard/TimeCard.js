ecd0c7130bd4a1b32a096bf18d7ef7035b10b8bb
a.urbanite <a.urbanite@gmail.com>
2022-10-13 15:00:53 +0200

    timeline css fixes

--- components/Career/TimeCard/TimeCard.js
+++ components/Career/TimeCard/TimeCard.js
@@ -5,10 +5,10 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 const TimeCard = ({timeLineEntry}) => {
 
   return (
-    <>
+    <div className={styles.container}>
       <h2 className={styles.title}>{timeLineEntry.fields.cardHeader}</h2>
       <>{documentToReactComponents(timeLineEntry.fields.cardText)}</>
-    </>
+    </div>
   )
 }
 
